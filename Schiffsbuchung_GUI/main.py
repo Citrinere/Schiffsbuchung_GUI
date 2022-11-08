ca892d182331eda91760a2a46af99b41c68ae1a6
Citrinere <Alex.1508@gmx.de>
2022-11-08 11:30:51 +0100

    Main
    Fenster in Main
    Interpreter change

--- Schiffsbuchung_GUI/main.py
+++ Schiffsbuchung_GUI/main.py
@@ -1,4 +1,16 @@
 # Python Project - Schiffsbuchung_GUI
 # Fuer das Modul Programmieren Graphischer Oberflaechen WS 2022
-# Projektmitglieder: Florian Czech, Alexander Schweizer
+# Projektmitglieder: Florian Czech, Alexander Teichmann, Alexander Schweizer
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+app = QApplication(sys.argv)
+
+w = QWidget()   #Fenster aufbauen != Fenster anzeigen
+
+w.show()    #Fenster anzeigen
+
+sys.exit(app.exec_())   #Python Programm stoppt wenn Fenster geschlossen wird
+
 
