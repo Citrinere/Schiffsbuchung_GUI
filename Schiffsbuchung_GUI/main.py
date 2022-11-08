241598c2c20991ebcacca61633119646cd6c7d35
Citrinere <Alex.1508@gmx.de>
2022-11-08 12:52:07 +0100

    Bilder und Main

--- Schiffsbuchung_GUI/main.py
+++ Schiffsbuchung_GUI/main.py
@@ -4,10 +4,14 @@
 
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
 
 app = QApplication(sys.argv)
 
 w = QWidget()   #Fenster aufbauen != Fenster anzeigen
+w.setGeometry(300,100,1000,700)  #(horizontale position, vertikale position, breite des Fensters, hoehe des Fensters)
+w.setWindowTitle("Schiffsbuchung")  #Fenstertitel setzen
+w.setWindowIcon(QIcon("Schifficon.png"))  #Pictogram/Favicon setzen
 
 w.show()    #Fenster anzeigen
 
